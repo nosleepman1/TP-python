9eff86ce611eafc5931f21d93e63257423819a20
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-02-23 15:12:48 +0000

    add principle menu

---
+++ src/views/menu.py
@@ -0,0 +1,21 @@
+
+
+def menuPrincipal():
+    print('=============MENU PRINCIPAL=============')
+    print('1. Ajouter un etudiant')
+    print('2. Afficher tous les etudiants')
+    print('3. Ajout d’une note')
+    print('4. Calcul des moyennes ')
+    print('5. Statistiques')
+    print('6. Sauvegarde')  
+    print('7. Export CSV')
+    print('8. Rechercher etudiant')
+    print('9. Suppression')
+    print('10. Modification')
+    print('11. Quiiter le programme')
+
+
+    choix = int(input("\nvotre choix: "))
+    return choix
+
+
